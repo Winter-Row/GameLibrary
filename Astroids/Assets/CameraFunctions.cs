36addad65b1652b84cbf0607cb551f3959099667
Winter-Row <winter.e.r@outlook.com>
2025-03-15 00:34:33 -0400

    Implemented ship movement
    Add ship movement and started working on keeping the ship within the bonds of the camera

---
+++ Astroids/Assets/CameraFunctions.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFunctions : MonoBehaviour
+{
+    private Camera camera;
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+        player = GameObject.Find("Ship");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Vector3 getPlayerCameraPos()
+    {
+        return camera.WorldToScreenPoint(player.transform.position);
+    }
+
+    public float getCameraBondHight()
+    {
+        
+        return camera.orthographicSize + gameObject.transform.position.y;
+    }
+
+    public float getCameraBondWidth()
+    {
+        return camera.orthographicSize + gameObject.transform.position.x;
+    }
+}
